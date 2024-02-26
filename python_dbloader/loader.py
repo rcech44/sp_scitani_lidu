4fef5b502fe30e7c3ccf30988ad526d7f9749cb3
Radomir Cech <rcech44@gmail.com>
2024-02-26 20:29:35 +0100

    Added new statistics

--- python_dbloader/loader.py
+++ python_dbloader/loader.py
@@ -1,14 +1,16 @@
 import pandas as pd
+import numpy as np
 from pymongo import MongoClient
 
-# Nastavení připojení k MongoDB
+# Configuration
 client = MongoClient('mongodb://localhost:27017')
 db = client['scitani_lidu']
 collection = db['obyvatelstvo_pohlavi_vek_5']
-
 csv_file_path = '../data_original/sldb2021_vek5_pohlavi.csv'
+csv_chunksize = 1000
 
-chunksize = 1000
-for chunk in pd.read_csv(csv_file_path, chunksize=chunksize):
+# Get data from CSV and insert into MongoDB
+for chunk in pd.read_csv(csv_file_path, chunksize=csv_chunksize):
+    chunk.replace({np.nan: None}, inplace=True)
     records = chunk.to_dict('records')
     collection.insert_many(records)
